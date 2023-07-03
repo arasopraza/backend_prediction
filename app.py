ebd9c5355134b8175e0b5dcaa09bba3c91564a39
arasopraza <aras.opraza@gmail.com>
2023-07-03 16:24:38 +0700

    feat: add input user for predict

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import joblib
 import numpy as np
 
@@ -6,10 +6,15 @@ app = Flask(__name__)
 
 model = joblib.load("komoditas.pkl")
 
-@app.route('/predict')
-def index():
-    X = np.array([1222, 0]).reshape(1, -1)
-    # make prediction using loaded KNN model
+app.run("0.0.0.0")
+@app.route('/predict', methods = ["POST"])
+def predict():
+    x1 = request.json["x1"]
+    x2 = request.json["x2"]
+
+    X = np.array([x1, x2]).reshape(1, -1)
+
+    # make prediction using loaded regresi model
     y_pred = model.predict(X)
     # return prediction as JSON response
     return jsonify({"prediction": int(y_pred[0])})
