e156c0f3a3aa4f46d79599b4a7b151fca667b703
arasopraza <aras.opraza@gmail.com>
2023-08-12 10:27:46 +0700

    fix: error file path

--- upload_data.py
+++ upload_data.py
@@ -9,7 +9,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
         
-upload_directory = 'be/data'
+upload_directory = '/data'
 
 response = {
     "message": "",
